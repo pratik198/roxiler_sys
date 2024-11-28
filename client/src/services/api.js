262c5061a148e52b595d337be54511d8a34f9ff2
Arun pratik <routpratik09@gmail.com>
2024-11-28 15:03:19 +0530

    changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api";
+const API_URL = "https://roxiler-sys-4.onrender.com";
 
 export const initializeDatabase = () => axios.get(`${API_URL}/initialize`);
 
